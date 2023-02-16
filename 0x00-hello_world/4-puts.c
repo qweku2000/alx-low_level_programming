806bb25d7becec39a59c0a1837fd728a45f26562
qweku2000 <joeadofonyarko@gmail.com>
2023-02-16 07:34:04 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /*
  * main the entry pount of the whole program
+ *
  * Return 0 if no error and a nonzero if error
 */
 
 
-int main(){
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
-    }
+int main()
+ {
+   puts("\"Programming is like building a multilingual puzzle");
+   return 0;
+ }
