8ec55ebd8514029ae9ffd7a854fa0b3f3ddfeaa3
qweku2000 <joeadofonyarko@gmail.com>
2023-02-18 06:40:58 -0800

    Negative positive number checking

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,11 +22,11 @@ int main(void)
         }
         else if (n < 0 )
         {
-           printf("%d is negative", n);
+           printf("%d is negative\n", n);
         }
         else
         {
-           printf("%d is positive", n);
+           printf("%d is positive\n", n);
         }
  
  return (0);
