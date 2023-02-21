000d74bb59925eaa26401803a1cdeb9dd9031d2d
qweku2000 <joeadofonyarko@gmail.com>
2023-02-21 02:07:57 -0800

    print alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-  prntf("_putchar");
-  return (0)
-}  
+  printf("_putchar\n");
+  return (0);
+}
+
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char case;
+  for( case = 'a'; case <= 'z'; case++)
+    {
+      putchar(case);
+    }
+  printf("\n");
+}   
