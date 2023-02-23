09cb260c3c55f810aaff818bc90cfb317eaa7a73
qweku2000 <joeadofonyarko@gmail.com>
2023-02-22 17:41:30 -0800

    last number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,35 @@
-#include "main.h"
 #include <stdio.h>
 
-
-int print_last_digit(int n)
+int print_last_digit(int num)
 {
-	int num;
+    if (num < 0)
+    {
+        int abs_num = -num;
+        if (abs_num % 10 < 0)
+        {
+            int abs_last_digit = -(abs_num % 10);
+            abs_num = abs_num / 10 * 10 + abs_last_digit;
+        }
+        _putchar(abs_num % 10 + '0');
+        return abs_num % 10;
+    }
+    else
+    {
+        _putchar(num % 10 + '0');
+        return num % 10;
+    }
+}
 
-	if (num%10 < 0)
-		num%10 *= -1;
+void test_islower(int n)
+{
+    int r;
+    r = _islower(n);
+    _putchar(r + '0');
+    _putchar('\n');
+}
 
-	 putchar(num%10 + '0');
-
-	return (last_digit);
+int main(void)
+{
+    test_islower('H');
+    return 0;
 }
