084b44bce9c4e693f1226de30ea19636b7c251da
qweku2000 <joeadofonyarko@gmail.com>
2023-11-27 04:29:47 -0800

    elf headers

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,11 +6,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
   int fd;
   ssize_t r,w;
-  char* buffer = malloc(sizeof(char)*letters)
+  char* buffer = malloc(sizeof(char)*letters);
+  
   if (!filename)
     {
       return (0);
-    }
+    };
 	
   fd = open(filename,O_RDONLY);
   if (fd<0)
@@ -23,12 +24,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
   r = read(fd,buffer,letters);
   
   if (r<0)
-    free(buffer);
-    return (0);
-
+    {
+      free(buffer);
+      return (0);
+    }
   w = write(STDOUT_FILENO,buffer,r);
+
   if (r!=w)
     return (0);
-  return (w)
+  return (w);
     
 }
